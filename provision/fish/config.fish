fde323289914b89e9c6bd27c30cf33119f265163
william chu <chudood@gmail.com>
2015-03-21 06:20:10 +0000

    fish env command to run emacs daemon if it is not started

--- provision/fish/config.fish
+++ provision/fish/config.fish
@@ -1,3 +1,13 @@
+function emacs
+		echo "Using emacs client/daemon loader in config.fish..."
+		if ps -ef | grep "emacs --daemon" | grep -v grep | grep (whoami)
+		 	env emacsclient -t $argv
+		else
+			emacs --daemon
+			env emacsclient -t $argv	
+		end
+end
+
 . "$HOME/.config/fish/fish_prompt.fish"
 . "$HOME/.config/fish/aliases.fish"
 . "$HOME/.config/fish/exports.fish"
